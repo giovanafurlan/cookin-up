f5b3b09ab63cf916375163cdfc6d96ad72392a79
giovanaFurlan <giovananelofurlan@gmail.com>
2024-02-20 22:06:46 -0300

    first commit

---
+++ src/components/SuaLista.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+export default {
+  props: {
+    ingredientes: { type: Object, required: true },
+  },
+};
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
+
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+      <li v-for="ingrediente in ingredientes" :key="ingrediente">
+        <Tag :texto="ingrediente" ativa="" />
+      </li>
+    </ul>
+    <p v-else class="paragrafo lista-vazia">
+      <img
+        src="../assets/imagens/icones/lista-vazia.svg"
+        alt="Ícone de pesquisa"
+      />
+      Sua lista está vazia, selecione ingredientes para iniciar.
+    </p>
+  </section>
+</template>
